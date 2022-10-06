52f797d76505d5e3c4d64fbddb6e48d918482a79
Kharel Valtingojer <valtingojer@gmail.com>
2022-10-06 21:39:31 +0100

    Fixed "no namespace" at script

--- Unity_C#_WeaponInterface/App/Sample/WeaponInterface/Scripts/Projectil/SelfDestroy.cs
+++ Unity_C#_WeaponInterface/App/Sample/WeaponInterface/Scripts/Projectil/SelfDestroy.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SelfDestroy : MonoBehaviour
+namespace App.Samples.WeaponInterface.Scripts
 {
-	[SerializeField]
-	[Range(0,4)]
-	private int delay = 2;
+	public class SelfDestroy : MonoBehaviour
+	{
+		[SerializeField]
+		[Range(0,4)]
+		private int delay = 2;
 	
     
-	// This function is called when the object becomes enabled and active.
-	protected void OnEnable()
-	{
-		if(delay == 0)
-			return;
-		Destroy(gameObject, delay);
+		// This function is called when the object becomes enabled and active.
+		protected void OnEnable()
+		{
+			if(delay == 0)
+				return;
+			Destroy(gameObject, delay);
+		}
 	}
+	
 }
